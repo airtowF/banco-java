6ca67e9de6aa55b398c8e1f87b96311061dfec87
AirtowF <airtow@protonmail.com>
2024-08-20 21:58:02 -0300

    Criado classe Conta, seus atributos e métodos depositar, sacar e transferir

---
+++ src/Conta.java
@@ -0,0 +1,28 @@
+public class Conta {
+    String titular;
+    double saldo;
+    int agencia;
+    int numero;
+
+    void depositar(double valor){
+        saldo += valor;
+    }
+
+    boolean sacar(double valor){
+        if (saldo >= valor) {
+            saldo -= valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    void transferir(double valor, Conta contaDestino){
+        boolean conseguiuSacar = this.sacar(valor);
+
+        if (conseguiuSacar){
+            contaDestino.depositar(valor);
+        }
+    }
+
+}
